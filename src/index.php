0dacc4e4fefc7e1127ffe82b2b5a29ff12425df9
Hahn, Ralf <Ralf.Hahn@h-da.de>
2020-04-16 10:22:23 +0200

    Studi-Ordner mit Praktikumslösungen jetzt auf der Einstiegsseite verlinkt

--- src/index.php
+++ src/index.php
@@ -9,9 +9,13 @@
 <h1>EWA using Docker-Containers</h1>
 </header>
 <main>
+<h2>General</h2>
 <p>This is the content of the file 'index.php' in the folder 'src'!</p>
-<p>Put your own code into that folder!</p>
-<p>For a working example you may have a look at <a href="./Klausuren">old EWA-examinations!</a>
+<p>Put your own code into that folder or subfolders!</p>
+<h2>Lab</h2>
+<p>Put the solution of your lab into the folders <em>Prak1 ... Prak5</em> within the folder <a href="./Praktikum">Praktikum</a></p>
+<h2>Old Examinations</h2>
+<p>For working examples you may have a look at <a href="./Klausuren">old EWA-examinations!</a></p>
 </main>
 </body>
 </html>
