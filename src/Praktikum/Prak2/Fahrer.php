6363b15a0a9e303b2efbdaf384ef8f25b44bbd73
ninhbui <ninh.bui@stud.h-da.de>
2020-05-25 23:17:48 +0700

    Fahrer PHP seite

---
+++ src/Praktikum/Prak2/Fahrer.php
@@ -0,0 +1,17 @@
+<?php // UTF-8 marker äöüÄÖÜß€
+require_once "./Page.php";
+
+class Fahrer extends Page {
+
+    protected function __constructor() {
+        parent::__constructor();
+    }
+
+    public function __destructor() {
+        parent::__destructor();
+    }
+
+    protected function processReceivedData() {
+        
+    }
+}
